23bf3af1f01a5091d03bd9184433a5bc68e7b7e1
Johan Lindkvist <johanlindkvist89@gmail.com>
2017-10-04 10:01:33 +0200

    Add files via upload

---
+++ Test.ino
@@ -0,0 +1,32 @@
+void setup()
+{
+  tone(4, 2000, 1000);
+  delay(1000);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  long tLeft = rcTime(8);
+
+  Serial.print("tLeft = ");
+  Serial.print(tLeft);
+  Serial.println(" us");
+
+  delay(1000);
+}
+
+long rcTime(int pin)
+{
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, HIGH);
+  delay(1);
+  pinMode(pin, INPUT);
+  digitalWrite(pin, LOW);
+  long time = micros();
+  while(digitalRead(pin));
+  time = micros() - time;
+  return time;
+}
+
